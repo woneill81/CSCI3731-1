2790d1ffe5a1f5256b60d7769de81cc9beea1df8
Wesley <woneill81@gmail.com>
2019-09-11 14:02:45 -1000

    updated

--- HW01/HW1.cc
+++ HW01/HW1.cc
@@ -1,15 +1,10 @@
 #include <cstdio>
 #include "subtraction.h"
 
-int subtraction(int x, int y)
-{
-	return x-y;
-}
-
 int main(int argc, char** argv)
 {
-
 int num1=10, num2=5;
+
 printf("%d\n", subtraction(num1,num2));
 
 }
---
+++ HW01/subtraction.cc
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "subtraction.h"
+
+int subtraction(int x, int y)
+{
+	return x-y;
+}
